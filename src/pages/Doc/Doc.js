9d9ccf4a43cf8d3a5b2756c03ac028d8c4581ac1
Riley Gramlich <101842921+rileygramlich@users.noreply.github.com>
2023-01-23 18:26:33 -0700

    navigate home after new doc

--- src/pages/Doc/Doc.js
+++ src/pages/Doc/Doc.js
@@ -1,55 +1,46 @@
 import { useState, useEffect, useRef, React } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Card } from "react-bootstrap";
 import { EditorState } from "draft-js";
 import { Editor } from "react-draft-wysiwyg";
 
 // import api-utils:
-import * as docsAPI from "../../utilities/docs-api"
+import * as docsAPI from "../../utilities/docs-api";
 
 // CSS
 import "./Doc.css";
 
 export default function Doc({ user }) {
-    // Initilize hooks for docs since we'll be doing client side, rendering.
-    // const [sections, setSections] = useState(['section 1', 'section 2'])
+  // Initilize hooks for docs since we'll be doing client side, rendering.
+  // const [sections, setSections] = useState(['section 1', 'section 2'])
 
-    
-    const [name, setName] = useState('')
-    const [content, setContent] = useState('')
-    
-    // const [doc, setDoc] = useState({
-    //     name: name,
-    //     content: content,
-    //     wordCount: content.split(' ').length
-    // })
+  const [name, setName] = useState("");
+  const [content, setContent] = useState("");
 
-    const docsRef = useRef([])
+  const navigate = useNavigate();
 
-    // useEffect(function () {
-    //     async function getDocs() {
-    //       const docs = await docsAPI.getAll();
-    //       docsRef.current = [
-    //         ...new Set(docs.map((doc) => doc.name)),
-    //       ];
-    //     }
-    //     getDocs();
-    //   }, []);
+  // const [doc, setDoc] = useState({
+  //     name: name,
+  //     content: content,
+  //     wordCount: content.split(' ').length
+  // })
 
+  const docsRef = useRef([]);
 
   async function handleSaveDoc(event) {
     event.preventDefault();
-    console.log('You are saving' + name + '   Content: ' + content)
-    let wordCount = content.split(' ').length
+    console.log("You are saving" + name + "   Content: " + content);
+    let wordCount = content.split(" ").length - 1;
     // console.log(doc)
-    docsAPI.create(name, content, wordCount)
+    docsAPI.create(name, content, wordCount);
+    navigate('/')
   }
 
   return (
     <main className="Doc">
       <h1 className="title">{user.name}'s Doc Page</h1>
       <div className="page-container">
-      <Editor/>
+        <Editor />
         <div className="sections-sidebar">
           {/* <a href="#" className="section">
             section 1
@@ -62,27 +53,35 @@ export default function Doc({ user }) {
           </a> */}
         </div>
         <form className="doc-form" onSubmit={handleSaveDoc}>
-          <input type="text" name={name} onChange={(e) => setName(e.target.value)}/>
-          <textarea name="content"  id="" cols="70" rows="30" onChange={(e) => setContent(e.target.value)}>
+          <input
+            type="text"
+            name={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <textarea
+            name="content"
+            id=""
+            cols="70"
+            rows="30"
+            onChange={(e) => setContent(e.target.value)}
+          >
             ...
           </textarea>
           <button type="submit">Save</button>
         </form>
         <div>
-            <h3>All Docs:</h3>
-            <Card style={{ width: "18rem" }}>
-          <Card.Body>
-            <Card.Title>{name}</Card.Title>
-            <Card.Subtitle className="mb-2 text-muted">
-              Words: {content.split(' ').length}
-            </Card.Subtitle>
-            <Card.Text>
-              {content}
-            </Card.Text>
-            <Card.Link>Continue Doc</Card.Link>
-            <Card.Link>Delete</Card.Link>
-          </Card.Body>
-        </Card>
+          <h3>All Docs:</h3>
+          <Card style={{ width: "18rem" }}>
+            <Card.Body>
+              <Card.Title>{name}</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">
+                Words: {content.split(" ").length}
+              </Card.Subtitle>
+              <Card.Text>{content}</Card.Text>
+              <Card.Link>Continue Doc</Card.Link>
+              <Card.Link>Delete</Card.Link>
+            </Card.Body>
+          </Card>
         </div>
       </div>
     </main>
