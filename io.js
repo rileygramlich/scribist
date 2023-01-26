ef432ce290ea8a6123e415a7a29b076a170d1b23
Riley Gramlich <101842921+rileygramlich@users.noreply.github.com>
2023-01-26 15:44:33 -0700

    deploying

--- io.js
+++ io.js
@@ -3,7 +3,7 @@ const Doc = require("./models/doc");
 
 const io = require("socket.io")(3002, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "http://localhost:3001",
     methods: ["GET", "POST"],
   },
 });
