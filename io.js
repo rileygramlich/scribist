89537ee20fae41bd4f39a11cb3c7c8ed6866ae56
Riley Gramlich <101842921+rileygramlich@users.noreply.github.com>
2023-01-26 22:08:12 -0700

    update socket origin

--- io.js
+++ io.js
@@ -3,7 +3,7 @@ const Doc = require("./models/doc");
 
 const io = require("socket.io")(3002, {
   cors: {
-    origin: "http://localhost:3001",
+    origin: "https://scribist.herokuapp.com",
     methods: ["GET", "POST"],
   },
 });
