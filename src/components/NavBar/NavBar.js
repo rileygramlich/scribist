bd8c5082773efefe8778fce52399a38492dfcb63
Riley Gramlich <101842921+rileygramlich@users.noreply.github.com>
2023-02-01 14:56:58 -0700

    update About page

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 // Bootstrap imports
 import Container from "react-bootstrap/Container";
@@ -8,7 +8,6 @@ import NavDropdown from "react-bootstrap/NavDropdown";
 
 import { MdDarkMode } from "react-icons/md";
 
-
 // CSS
 import "../../pages/App/App.css";
 import "./NavBar.css";
@@ -16,7 +15,6 @@ import "./NavBar.css";
 import * as userService from "../../utilities/users-service";
 
 export default function NavBar({ user, setUser, toggleTheme, handleNewDoc }) {
-
   function handleLogout() {
     userService.logout();
     setUser(null);
@@ -35,69 +33,75 @@ export default function NavBar({ user, setUser, toggleTheme, handleNewDoc }) {
     <Navbar className="Navbar" expand="lg" id="Nav">
       <Container className="container-fluid">
         <div className="logo-n-button">
-        <Navbar.Brand className="logo-contain">
-          <Link className="logo" to="/">
-            Scribist
-          </Link>
-        </Navbar.Brand>
-        <button className="dark-toggle" onClick={toggleTheme}>
-        <MdDarkMode/>
-        </button>
+          <Navbar.Brand className="logo-contain">
+            <Link className="logo" to="/">
+              Scribist
+            </Link>
+          </Navbar.Brand>
+          <button className="dark-toggle" onClick={toggleTheme}>
+            <MdDarkMode />
+          </button>
         </div>
         <form className="search">
           {/* <input type="text" name="search" placeholder="search ..." />
           Add search functionality */}
         </form>
         <div>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="container-fluid">
-            <NavDropdown
-              className="ml-auto"
-              title="Utilities"
-              id="basic-nav-dropdown"
-              show={show}
-              onMouseEnter={showDropdown}
-              onMouseLeave={hideDropdown}
-            >
-              <div className="drop-contain">
-                <NavDropdown.Item className="drop-item">
-                  { user ? <button className='new-doc-btn' onClick={handleNewDoc}>New Doc</button> : <Link to="/">Login</Link>}
-                    
-                </NavDropdown.Item>
-                {/* <NavDropdown.Item className="drop-item">
-                  <Link to="/notes/new">New Note</Link>
-                </NavDropdown.Item> */}
-                <NavDropdown.Item className="drop-item">
-                  <Link to="/berserk">Berserk Mode</Link>
-                </NavDropdown.Item>
-                <NavDropdown.Item className="drop-item">
-                  <Link to="/typetest">Test Typing Speed</Link>
-                </NavDropdown.Item>
-              </div>
-            </NavDropdown>
-            {user ? (
-              <NavDropdown title="User" id="basic-nav-dropdown">
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="container-fluid">
+              <NavDropdown
+                className="ml-auto"
+                title="Utilities"
+                id="basic-nav-dropdown"
+                show={show}
+                onMouseEnter={showDropdown}
+                onMouseLeave={hideDropdown}
+              >
                 <div className="drop-contain">
-                  <div>User: {user.name}</div>
-                  <div>Email: {user.email}</div>
-                <NavDropdown.Divider />
-                  <NavDropdown.Item >
-                    <Link to="/user/logout" onClick={handleLogout}>
-                      Logout
-                    </Link>
+                  <NavDropdown.Item className="drop-item">
+                    {user ? (
+                      <button className="new-doc-btn" onClick={handleNewDoc}>
+                        New Doc
+                      </button>
+                    ) : (
+                      <Link to="/">Login</Link>
+                    )}
+                  </NavDropdown.Item>
+
+                  <NavDropdown.Item className="drop-item">
+                    <Link to="/berserk">Berserk Mode</Link>
+                  </NavDropdown.Item>
+                  <NavDropdown.Item className="drop-item">
+                    <Link to="/typetest">Test Typing Speed</Link>
+                  </NavDropdown.Item>
+                  <NavDropdown.Item className="drop-item">
+                    <Link to="/about">About</Link>
                   </NavDropdown.Item>
                 </div>
               </NavDropdown>
-            ) : (
-              <NavDropdown title="User" id="basic-nav-dropdown">
-                <NavDropdown.Item >
-                  <Link to="/">Login</Link>
-                </NavDropdown.Item>
-              </NavDropdown>
-            )}
-          </Nav>
-        </Navbar.Collapse>
+              {user ? (
+                <NavDropdown title="User" id="basic-nav-dropdown">
+                  <div className="drop-contain">
+                    <div>User: {user.name}</div>
+                    <div>Email: {user.email}</div>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item>
+                      <Link to="/user/logout" onClick={handleLogout}>
+                        Logout
+                      </Link>
+                    </NavDropdown.Item>
+                  </div>
+                </NavDropdown>
+              ) : (
+                <NavDropdown title="User" id="basic-nav-dropdown">
+                  <NavDropdown.Item>
+                    <Link to="/">Login</Link>
+                  </NavDropdown.Item>
+                </NavDropdown>
+              )}
+            </Nav>
+          </Navbar.Collapse>
         </div>
       </Container>
     </Navbar>
--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -2,20 +2,62 @@ import React from "react";
 import { FaGithub, FaTwitter, FaLinkedin } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
+import "./About.css";
+
 export default function About() {
   return (
     <div className="About">
       <h1 className="title">About Scribist</h1>
-      <p className="about-content">
-        Scribist was designed and developed by Riley Gramlich, a full-stack
-        developer from Alberta, Canada.
-      </p>
-      <div className="waves">
-          <div className="wave" id="wave1"></div>
-          <div className="wave" id="wave2"></div>
-          <div className="wave" id="wave3"></div>
-          <div className="wave" id="wave4"></div>
-        </div>
+      <div className="main-content">
+        <h3 className="note">A note from the developer:</h3>
+        <hr />
+        <p className="about-content">
+          Hi, my name is Riley Gramlich. I am a full-stack software developer
+          from Alberta, Canada. I am passionate and motivated to make useful
+          applications such as this one. Scribist was really born out of my
+          desire to have an all in one place to create and edit documents, use a
+          feature like Berserk mode to output more in less time (which I'm using
+          right now to write this), and to be able to test your typing speed in
+          a fun way!
+          <hr />
+          Scribist features web socket integration which allows you to work
+          collaboratively on a document with another user! Just shoot them the
+          URL and write away!
+          <hr />
+          As the creator of this web app, I am open to criticism and feedback.
+          If you notice any problems with the site, bugs, etc, or have
+          suggestions of what you'd like to see implemented to improve it,
+          please send me a message on <Link
+                rel="opener"
+                target="_blank"
+                to="https://www.linkedin.com/in/rileygramlich/"
+                className="link"
+              >
+                LinkedIn
+              </Link> or <Link
+                rel="opener"
+                target="_blank"
+                to="https://twitter.com/rileygramlich"
+                className="link"
+              >
+                Twitter
+              </Link> about it!
+          <hr />
+          If you enjoy Scribist, I'd love to connect with you! Below you can
+          find links to my LinkedIn and Twitter account. Also, if you're
+          interested in checking out other projects I've worked on, be sure to
+          check on my Portfolio and my Github page (also linked below). Thanks
+          for using Scribist and have a wonderful day!<hr />
+        </p>
+        <h4 className="sign">~ <Link
+                rel="opener"
+                target="_blank"
+                to="https://rileygramlich.github.io/riley-gramlich-portfolio/"
+              >
+                Riley Gramlich
+              </Link> </h4>
+      </div>
+      <footer>
         <ul className="social-icon">
           <li className="social-icon__item">
             <div className="social-icon__link"></div>
@@ -40,7 +82,6 @@ export default function About() {
               >
                 <FaLinkedin />
               </Link>
-              <i className="fa-regular fa-house"></i>
             </div>
           </li>
           <li className="social-icon__item">
@@ -83,7 +124,7 @@ export default function About() {
             </div>
           </li>
         </ul>
-        {/* <a target="_blank" rel="stylesheet" href="https://github.com/rileygramlich">My Github</a> */}
+      </footer>
     </div>
   );
 }
