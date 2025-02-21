17653b855387002060b363ff0158fe9e61ba8f0f
Riley Gramlich <101842921+rileygramlich@users.noreply.github.com>
2025-02-21 13:40:37 -0700

    prepare for production

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -3,53 +3,53 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 
 module.exports = {
-  create,
-  login,
-  checkToken,
+    create,
+    login,
+    checkToken,
 };
 
 /*-- Helper Functions --*/
 
 function createJWT(user) {
-  console.log("ceating-jwt");
-  return jwt.sign(
-    // data payload
-    { user },
-    process.env.SECRET,
-    { expiresIn: "24h" }
-  );
+    console.log("ceating-jwt");
+    return jwt.sign(
+        // data payload
+        { user },
+        process.env.SECRET,
+        { expiresIn: "24h" }
+    );
 }
 
 async function create(req, res) {
-  try {
-    console.log("creating");
-    const user = await User.create(req.body);
-    const token = createJWT(user);
-    res.status(200).json(token);
-  } catch (err) {
-    console.log(err);
-    res.status(400).json(err);
-  }
+    try {
+        console.log("creating");
+        const user = await User.create(req.body);
+        const token = createJWT(user);
+        res.status(200).json(token);
+    } catch (err) {
+        console.log(err);
+        res.status(400).json(err);
+    }
 }
 
 async function login(req, res) {
-  try {
-    const user = await User.findOne({ email: req.body.email });
-    if (!user) throw new Error("No matching user");
-    const match = await bcrypt.compare(req.body.password, user.password);
-    if (!match) throw Error("wrong password");
-    const token = createJWT(user);
-    res.status(200).json(token);
-  } catch (err) {
-    console.log(err);
-    res.status(400).json(err);
-  }
+    try {
+        const user = await User.findOne({ email: req.body.email });
+        if (!user) throw new Error("No matching user");
+        const match = await bcrypt.compare(req.body.password, user.password);
+        if (!match) throw Error("wrong password");
+        const token = createJWT(user);
+        res.status(200).json(token);
+    } catch (err) {
+        console.log(err);
+        res.status(400).json(err);
+    }
 }
 
 // controllers/api/users.js
 
 function checkToken(req, res) {
-  // req.user will always be there for you when a token is sent
-  console.log("req.user", req.user);
-  res.status(200).json(req.exp);
+    // req.user will always be there for you when a token is sent
+    console.log("req.user", req.user);
+    res.status(200).json(req.exp);
 }
